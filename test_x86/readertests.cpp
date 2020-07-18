77879e8985da199e44aa4785cf656329a1e331e3
Newdigate <nicnewdigate@gmail.com>
2020-07-18 11:32:49 +0100

    improve test

--- test_x86/readertests.cpp
+++ test_x86/readertests.cpp
@@ -15,41 +15,36 @@ BOOST_AUTO_TEST_SUITE(AbsoluteDateCheckSuite)
 
     BOOST_FIXTURE_TEST_CASE(ReadForwardAtRegularPlaybackRate, ResamplingReaderFixture) {
 
-        const uint32_t testFileSize = 800;
-        printf("test_resampling_forward(%d)\n", testFileSize);
-
-        int16_t testbuffer[testFileSize];
-        for (int16_t i = 0; i < testFileSize; i++) {
-            testbuffer[i] = i;
+        const uint32_t expectedDataSize = 800;
+        printf("ReadForwardAtRegularPlaybackRate(%d)\n", expectedDataSize);
+        int16_t expected[expectedDataSize];
+        for (int16_t i = 0; i < expectedDataSize; i++) {
+            expected[i] = i;
         }
-
-        SD.setSDCardFileData( (char*) testbuffer, testFileSize * 2);
+        SD.setSDCardFileData((char*) expected, expectedDataSize * 2);
 
         resamplingSdReader->begin();
         resamplingSdReader->setPlaybackRate(1.0);
         resamplingSdReader->play("test2.bin");
         resamplingSdReader->setLoopType(looptype_none);
-        int16_t buffer[256];
+        int16_t actual[1024];
 
         int j = 0, bytesRead = 0, total_bytes_read = 0;
         do {
-            bytesRead = resamplingSdReader->read( buffer, 512 );
+            bytesRead = resamplingSdReader->read(&actual[j * 256], 512 );
             total_bytes_read += bytesRead;
             printf("j:%d bytesRead: %d \n", j, bytesRead);
 
             for (int i=0; i < bytesRead/2; i++) {
-                printf("\t\t[%x]:%x", i, buffer[i]);
+                printf("\t\t[%x]:%x", expected[j * 256 + i], actual[i]);
             }
-            //char c = cin.get();
             printf("\n");
             j++;
         } while (bytesRead > 0);
         printf("total_bytes_read: %d \n", total_bytes_read);
-
-
         resamplingSdReader->close();
-        int i = 1;
-        BOOST_CHECK_EQUAL(i,1);
+
+        BOOST_CHECK_EQUAL_COLLECTIONS(&expected[0], &expected[expectedDataSize-1], &actual[0], &actual[(total_bytes_read / 2)-1]);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
