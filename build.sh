5b7ad952929f4fd857dfc048a49e8ad3b854a44e
Newdigate <nicnewdigate@gmail.com>
2021-05-18 11:43:11 +0100

    fix build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cd examples/basic
 rm -rf cmake-build-debug
 mkdir cmake-build-debug
 cd cmake-build-debug
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DOPT_BUILD_FOR_LINUX=OFF ..
 make
