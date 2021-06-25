3eeaeec11cf1a9b02ef3bb7870268d9a8b02e728
Newdigate <nicnewdigate@gmail.com>
2021-06-25 09:57:30 +0100

    library.json + properties

--- teensy_sampler.h
+++ teensy_sampler.h
@@ -2,12 +2,14 @@
 // Created by Nicholas Newdigate on 21/07/2020.
 //
 
-#ifndef TEENSY_RESAMPLING_SDREADER_TEENSY_SAMPLER_H
-#define TEENSY_RESAMPLING_SDREADER_TEENSY_SAMPLER_H
+#ifndef TEENSY_RESAMPLING_ARDUINO_SAMPLER_H
+#define TEENSY_RESAMPLING_ARDUINO_SAMPLER_H
 
 #include "src/waveheaderparser.h"
 #include "src/ResamplingSdReader.h"
+#include "src/ResamplingArrayReader.h"
 #include "src/playsdrawresmp.h"
 #include "src/playsdwavresmp.h"
+#include "src/playarrayresmp.h"
 
-#endif //TEENSY_RESAMPLING_SDREADER_TEENSY_SAMPLER_H
+#endif //TEENSY_RESAMPLING_ARDUINO_SAMPLER_H
