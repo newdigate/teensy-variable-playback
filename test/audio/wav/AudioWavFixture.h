586d6dd0dd3ae50ef12a16a29efaf9d6e7faae2c
Newdigate <nicnewdigate@gmail.com>
2021-07-10 18:55:20 +0100

    add some tests for wav audio resampling

---
+++ test/audio/wav/AudioWavFixture.h
@@ -0,0 +1,25 @@
+//
+// Created by Nicholas Newdigate on 17/06/2021.
+//
+#ifndef TEENSY_RESAMPLING_SDREADER_RESAMPLINGWAVFIXTURE_H
+#define TEENSY_RESAMPLING_SDREADER_RESAMPLINGWAVFIXTURE_H
+
+#include <Arduino.h>
+#include <Audio.h>
+#include <SD.h>
+#include <iterator>
+#include "output_test.h"
+#include "playsdwavresmp.h"
+
+struct AudioWavFixture {
+
+    AudioWavFixture()  {
+        AudioMemory(20);
+    }
+
+    ~AudioWavFixture() {
+        arduino_should_exit = true;
+    }
+};
+
+#endif //TEENSY_RESAMPLING_SDREADER_RESAMPLINGWAVFIXTURE_H
---
+++ test/audio/wav/test_wav_mono_loop_forward_playback.cpp
@@ -0,0 +1,236 @@
+//
+// Created by Nicholas Newdigate on 18/07/2020.
+//
+
+#ifndef TEENSY_AUDIO_SDWAV_MONO_READERTESTS_CPP
+#define TEENSY_AUDIO_SDWAV_MONO_READERTESTS_CPP
+
+#include <boost/test/unit_test.hpp>
+#include "AudioWavFixture.h"
+
+BOOST_AUTO_TEST_SUITE(test_audio_wav_mono_loop_forward_playback)
+
+    const uint16_t numberOfChannels = 1;
+    BOOST_FIXTURE_TEST_CASE(Wav_fwd_1_0000_quadratic_mono_noloop, AudioWavFixture) {
+
+        // GUItool: begin automatically generated code
+        AudioPlaySdWavResmp      wave;        //xy=306,225
+        TestAudioOutput          testout;       //xy=612,224
+        AudioConnection          patchCord1(wave, 0, testout, 0);
+        // GUItool: end automatically generated code
+
+        const double playbackRate = 1.0;
+        const std::string testName = "Wav_fwd_1_0000_quadratic_mono_noloop";
+        const std::string outputFile = testName+".wav";
+        const std::string outputFileName = "output/" + outputFile;
+        const std::string referencePath = "test/resources/reference/";
+        const std::string referenceFileName = referencePath + testName + ".wav";
+        SD.setSDCardFolderPath(referencePath);
+        testout.saveOutputFile(outputFile.c_str());
+        
+        wave.begin();
+        wave.enableInterpolation(true);
+        wave.setPlaybackRate(playbackRate);
+        wave.play("kick.wav");
+        while (wave.isPlaying()) {
+            testout.isr();
+        }
+        testout.closeOutputfile(numberOfChannels);
+        patchCord1.disconnect();
+        AudioConnection::reset();
+        arduino_should_exit = true;
+        
+        std::ifstream ifs1(outputFileName);
+        std::ifstream ifs2(referenceFileName);
+        std::istream_iterator<char> b1(ifs1), e1;
+        std::istream_iterator<char> b2(ifs2), e2;
+
+        BOOST_CHECK_EQUAL_COLLECTIONS(b1, e1, b2, e2);
+    }
+
+    BOOST_FIXTURE_TEST_CASE(Wav_fwd_0_5000_quadratic_mono_noloop, AudioWavFixture) {
+
+        // GUItool: begin automatically generated code
+        AudioPlaySdWavResmp        wave;        //xy=306,225
+        TestAudioOutput          testout;       //xy=612,224
+        AudioConnection          patchCord1(wave, 0, testout, 0);
+        // GUItool: end automatically generated code
+        const double playbackRate = 0.5;
+        const std::string testName = "Wav_fwd_0_5000_quadratic_mono_noloop";
+        const std::string outputFile = testName+".wav";
+        const std::string outputFileName = "output/" + outputFile;
+        const std::string referencePath = "test/resources/reference/";
+        const std::string referenceFileName = referencePath + testName + ".wav";
+        SD.setSDCardFolderPath(referencePath);
+
+        testout.saveOutputFile(outputFile.c_str());
+        wave.begin();
+        wave.enableInterpolation(true);
+        wave.setPlaybackRate(playbackRate);
+        wave.play("kick.wav");
+        while (wave.isPlaying()) {
+            testout.isr();
+        }
+        testout.closeOutputfile(numberOfChannels);
+        patchCord1.disconnect();
+        AudioConnection::reset();
+        arduino_should_exit = true;
+
+        std::ifstream ifs1(outputFileName);
+        std::ifstream ifs2(referenceFileName);
+        std::istream_iterator<char> b1(ifs1), e1;
+        std::istream_iterator<char> b2(ifs2), e2;
+
+        BOOST_CHECK_EQUAL_COLLECTIONS(b1, e1, b2, e2);
+    }
+
+    BOOST_FIXTURE_TEST_CASE(Wav_fwd_2_0000_quadratic_mono_noloop, AudioWavFixture) {
+
+        // GUItool: begin automatically generated code
+        AudioPlaySdWavResmp        wave;        //xy=306,225
+        TestAudioOutput          testout;       //xy=612,224
+        AudioConnection          patchCord1(wave, 0, testout, 0);
+        // GUItool: end automatically generated code
+
+        const double playbackRate = 2.0;
+        const std::string testName = "Wav_fwd_2_0000_quadratic_mono_noloop";
+        const std::string outputFile = testName+".wav";
+        const std::string outputFileName = "output/" + outputFile;
+        const std::string referencePath = "test/resources/reference/";
+        const std::string referenceFileName = referencePath + testName + ".wav";
+        SD.setSDCardFolderPath(referencePath);
+
+        testout.saveOutputFile(outputFile.c_str());     
+        wave.begin();           
+        wave.enableInterpolation(true);
+        wave.setPlaybackRate(playbackRate);
+        wave.play("kick.wav");
+        while (wave.isPlaying()) {
+            testout.isr();
+        }
+        testout.closeOutputfile(numberOfChannels);
+        patchCord1.disconnect();
+        AudioConnection::reset();
+        arduino_should_exit = true;
+
+        std::ifstream ifs1(outputFileName);
+        std::ifstream ifs2(referenceFileName);
+        std::istream_iterator<char> b1(ifs1), e1;
+        std::istream_iterator<char> b2(ifs2), e2;
+
+        BOOST_CHECK_EQUAL_COLLECTIONS(b1, e1, b2, e2);
+    }
+
+    BOOST_FIXTURE_TEST_CASE(Wav_fwd_0_7437_quadratic_mono_noloop, AudioWavFixture) {
+
+        // GUItool: begin automatically generated code
+        AudioPlaySdWavResmp        wave;        //xy=306,225
+        TestAudioOutput          testout;       //xy=612,224
+        AudioConnection          patchCord1(wave, 0, testout, 0);
+        // GUItool: end automatically generated code
+
+        const double playbackRate = 0.7437;
+        const std::string testName = "Wav_fwd_0_7437_quadratic_mono_noloop";
+        const std::string outputFile = testName+".wav";
+        const std::string outputFileName = "output/" + outputFile;
+        const std::string referencePath = "test/resources/reference/";
+        const std::string referenceFileName = referencePath + testName + ".wav";
+        SD.setSDCardFolderPath(referencePath);
+
+        testout.saveOutputFile(outputFile.c_str());
+        wave.begin();
+        wave.enableInterpolation(true);
+        wave.setPlaybackRate(playbackRate);
+        wave.play("kick.wav");
+        while (wave.isPlaying()) {
+            testout.isr();
+        }
+        testout.closeOutputfile(numberOfChannels);
+        patchCord1.disconnect();
+        AudioConnection::reset();
+        arduino_should_exit = true;
+
+        std::ifstream ifs1(outputFileName);
+        std::ifstream ifs2(referenceFileName);
+        std::istream_iterator<char> b1(ifs1), e1;
+        std::istream_iterator<char> b2(ifs2), e2;
+
+        BOOST_CHECK_EQUAL_COLLECTIONS(b1, e1, b2, e2);
+
+    }
+
+    BOOST_FIXTURE_TEST_CASE(Wav_fwd_1_7437_quadratic_mono_noloop, AudioWavFixture) {
+
+        // GUItool: begin automatically generated code
+        AudioPlaySdWavResmp        wave;        //xy=306,225
+        TestAudioOutput          testout;       //xy=612,224
+        AudioConnection          patchCord1(wave, 0, testout, 0);
+        // GUItool: end automatically generated code
+
+        const double playbackRate = 1.7437;
+        const std::string testName = "Wav_fwd_1_7437_quadratic_mono_noloop";
+        const std::string outputFile = testName+".wav";
+        const std::string outputFileName = "output/" + outputFile;
+        const std::string referencePath = "test/resources/reference/";
+        const std::string referenceFileName = referencePath + testName + ".wav";
+        SD.setSDCardFolderPath(referencePath);
+
+        testout.saveOutputFile(outputFile.c_str());
+        wave.begin();
+        wave.enableInterpolation(true);
+        wave.setPlaybackRate(playbackRate);
+        wave.play("kick.wav");
+        while (wave.isPlaying()) {
+            testout.isr();
+        }
+        testout.closeOutputfile(numberOfChannels);
+        patchCord1.disconnect();
+        AudioConnection::reset();
+        arduino_should_exit = true;
+
+        std::ifstream ifs1(outputFileName);
+        std::ifstream ifs2(referenceFileName);
+        std::istream_iterator<char> b1(ifs1), e1;
+        std::istream_iterator<char> b2(ifs2), e2;
+
+        BOOST_CHECK_EQUAL_COLLECTIONS(b1, e1, b2, e2);
+    }
+
+    BOOST_FIXTURE_TEST_CASE(Wav_fwd_8_7437_quadratic_mono_noloop, AudioWavFixture) {
+
+        // GUItool: begin automatically generated code
+        AudioPlaySdWavResmp        wave;        //xy=306,225
+        TestAudioOutput          testout;       //xy=612,224
+        AudioConnection          patchCord1(wave, 0, testout, 0);
+        // GUItool: end automatically generated code
+
+        const double playbackRate = 8.7437;
+        const std::string testName = "Wav_fwd_8_7437_quadratic_mono_noloop";
+        const std::string outputFile = testName+".wav";
+        const std::string outputFileName = "output/" + outputFile;
+        const std::string referenceFileName = "test/resources/reference/"+testName+".wav";
+
+        testout.saveOutputFile(outputFile.c_str());
+        wave.begin();        
+        wave.enableInterpolation(true);
+        wave.setPlaybackRate(playbackRate);
+        wave.play("kick.wav");
+        while (wave.isPlaying()) {
+            testout.isr();
+        }
+        testout.closeOutputfile(numberOfChannels);
+        patchCord1.disconnect();
+        AudioConnection::reset();
+        arduino_should_exit = true;
+
+        std::ifstream ifs1(outputFileName);
+        std::ifstream ifs2(referenceFileName);
+        std::istream_iterator<char> b1(ifs1), e1;
+        std::istream_iterator<char> b2(ifs2), e2;
+
+        BOOST_CHECK_EQUAL_COLLECTIONS(b1, e1, b2, e2);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif //TEENSY_AUDIO_SDWAV_MONO_READERTESTS_CPP
