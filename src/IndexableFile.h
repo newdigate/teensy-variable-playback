e0e96d0d11cc0ff0c6a43cf0e092928026333c4d
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2023-04-08 09:34:55 +0100

    Add some comments - no functional changes

--- src/IndexableFile.h
+++ src/IndexableFile.h
@@ -38,22 +38,37 @@ public:
     virtual ~IndexableFile() {
         close();
     }
-
+	
+	/*
+	 Retrieve a single sample from an "indexed file".
+	 
+	 By using a set of buffers as an in-memory cache, we aim to retrieve from the
+	 cache most of the time, and only do a file read on a cache miss.
+	 
+	 Note that the index is independent of the channel count.
+	*/
     int16_t &operator[](int i) {
         int32_t indexFor_i = i >> buffer_to_index_shift;
-        indexedbuffer *match = find_with_index(indexFor_i);
-        if (match == nullptr) {
-            if (_buffers.size() > MAX_NUM_BUFFERS - 1) {
-                indexedbuffer *first = _buffers[0];
-                _buffers.erase(_buffers.begin());
-                delete [] first->buffer;
-                delete first;
+        indexedbuffer *match = find_with_index(indexFor_i); // find which buffer has the sample
+		
+        if (match == nullptr)  // none of the buffers contains the required sample
+		{
+            if (_buffers.size() > MAX_NUM_BUFFERS - 1) // reached limit of number of allowed buffers
+			{
+                indexedbuffer* first = _buffers[0]; // assume the oldest buffer is no longer needed
+                _buffers.erase(_buffers.begin());	// remove from vector
+                delete [] first->buffer;			// delete buffer memory
+                delete first;						// and indexedbuffer object
             }
-            indexedbuffer *next = new indexedbuffer();
-            next->index = indexFor_i;
-            next->buffer = new int16_t[BUFFER_SIZE];
+            indexedbuffer *next = new indexedbuffer();  // create new indexedbuffer object
+            next->index = indexFor_i;					// say which samples it'll have in it
+            next->buffer = new int16_t[BUFFER_SIZE];	// allocate space for the samples
+			
+			// figure out file position to load into the buffer
             size_t basePos = indexFor_i << buffer_to_index_shift;
             size_t seekPos = basePos * element_size;
+			
+			// load the sample data from the file: note BUFFER_SIZE is in samples
             _file.seek(seekPos);
             int16_t bytesRead = _file.read(next->buffer, BUFFER_SIZE * element_size);
             #ifndef TEENSYDUINO
@@ -62,24 +77,34 @@ public:
                 _file = open(_filename);
             }
             #endif
+			
+			// fill in remaining indexedbuffer information: at the end of the file
+			// we may not have enough samples to fill the buffer.
             next->buffer_size = bytesRead;
+			
+			// add the newly-loaded indexedbuffer at the back of the vector: it's the newest one
             _buffers.push_back(next);
+			
+			// we now have a buffer with the desired sample in it
             match = next;
         }
         return match->buffer[i % BUFFER_SIZE];
     }
 
     void close() {
+		// close file if open
         if (_file.available()) {
             _file.close();
         }
 
-       for (auto && x : _buffers){
+		// delete all buffered data and clear the vector
+		for (auto && x : _buffers){
             delete [] x->buffer;
             delete x;
         }
         _buffers.clear();
 
+		// delete the filename
         if (_filename != nullptr) {
             delete [] _filename;
             _filename = nullptr;
@@ -91,6 +116,9 @@ protected:
     char *_filename;
     std::vector<indexedbuffer*> _buffers;
 
+	// search vector of buffers for the one with a 
+	// specific sample (set) in it.
+	// return pointer to correct indexedbuffer, or nullptr if not buffered
     indexedbuffer* find_with_index(uint32_t i) {
         for (auto && x : _buffers){
             if (x->index == i) {
